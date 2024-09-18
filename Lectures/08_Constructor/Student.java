67bc2b482f3bea6c90ea67795a3bc00bb0522211
Yaswanth <104082730+yaswanth1110@users.noreply.github.com>
2024-09-18 16:50:07 +0530

    Add files via upload

--- Lectures/08_Constructor/Student.java
+++ Lectures/08_Constructor/Student.java
@@ -3,46 +3,36 @@ package com.flm.core;
 public class Student {
 	private int code;
 	private double salary;
-	
-	public Student(){
-		code=-1;
-		salary=-500;
-	}
-	public Student(int a, double b){
-		code=a;
-		salary=b;
-	}
-	public Student(int a, int b){
-		code=a;
-		salary=(double)b;
-	}
-	public Student(double a, double b){
-		code=(int)a;
-		salary=b;
-	}
-	public Student(double b, int a){
-		code=a;
-		salary=b;
-	}
-	public Student(double b){
-		salary=b;
-	}
-	public Student(int a){
-		code=a;
-	}
-	public void setCode(int a) {
-		code = a;
-	}
 	public int getCode() {
 		return code;
 	}
-	
-	public void setSalary(double b) {
-		salary = b;
+	public void setCode(int code) {
+		this.code = code;
 	}
-	
 	public double getSalary() {
 		return salary;
 	}
-	
+	public Student(int code, double salary) {
+		System.out.println("code,salary is executed...");
+		this.code = code;
+		this.salary = salary;
+	}
+	public Student(double salary, int code) {
+		System.out.println("salary,code is executed...");
+		this.code = code;
+		this.salary = salary;
+	}
+	public Student() {}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public void display() {
+		System.out.println("Hello world...");
+	}
+	public void display(int code) {
+		System.out.println("Code : "+code);
+	}
+	public void display(double salary) {
+		System.out.println("Salary : "+salary);
+	}
 }
--- Lectures/08_Constructor/test/Test.java
+++ Lectures/08_Constructor/test/Test.java
@@ -5,11 +5,13 @@ import com.flm.core.Student;
 public class Test {
 
 	public static void main(String[] args) {
-		Student st = new Student(12,1.2);
-		//st.setCode(123);
-		//st.setSalary(1200.25);
-		System.out.println(st.getCode());
-		System.out.println(st.getSalary());
+		Student s1 = new Student(100,1200.25);
+		//s1.setCode(200);
+		//s1.setSalary(4000.45);
+		System.out.println(s1.getCode()+"-->"+s1.getSalary());
+		s1.display();
+		s1.display(1200.12);
+		s1.display(500);
 	}
 
 }
